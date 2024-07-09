6f90a6bfc7bb96cc71ffa2191ad1dae08420370e
mariannacoppa <densimetra@gmail.com>
2024-07-09 18:34:46 +0200

    [UPD] created five random numbers

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
 // creo array che conterrà 5 numeri casuali
 let randomNumbersArray = [];
-
+// ciclo l'array generando 5 numeri casuali
+for (let i = 0; i < 5; i++) {
+    randomNumbersArray[i] = Math.floor(Math.random() * 100 + 1);
+    console.log(randomNumbersArray[i]);
+}
